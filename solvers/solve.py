14cff71aec3257ab904d80473442e9532be6da94
MilkFansHelloWorld <31027215+MilkFansHelloWorld@users.noreply.github.com>
2021-02-24 22:28:19 +0000

    push simple solver

--- solvers/solve.py
+++ solvers/solve.py
@@ -12,8 +12,40 @@ def solve(inp, args):
     # TODO: Solve the problem
     random.seed(args['seed'])
     ns = parse(inp)
+    left = [pizza['id'] for pizza in ns.pizzas]
+    team_pizzas = []
+    for _ in range(ns.T4):
+        # pick pizzas
+        if len(left)<4:
+            break
+        pizza_list = []
+        for _ in range(4):
+            pizza_list.append(left.pop())
+        team_pizzas.append(pizza_list)
+    
+    for _ in range(ns.T3):
+        # pick pizzas
+        if len(left)<3:
+            break
+        pizza_list = []
+        for _ in range(3):
+            pizza_list.append(left.pop())
+        team_pizzas.append(pizza_list)
 
-    return '0'
+    for _ in range(ns.T2):
+        # pick pizzas
+        if len(left)<2:
+            break
+        pizza_list = []
+        for _ in range(2):
+            pizza_list.append(left.pop()) 
+        team_pizzas.append(pizza_list)
+    out = []
+    out.append(str(len(team_pizzas)))
+    for row in team_pizzas:
+        out.append(' '.join(map(str, [len(row)]+row)))
+    res = '\n'.join(out)
+    return res
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
